e883ba425cd98fd9f2974ab066eba2688c00f880
hiro0118 <fuji.hiroshi0118@gmail.com>
2023-01-21 01:54:52 +0900

    Add extension code

---
+++ skip-ad.js
@@ -0,0 +1,31 @@
+{
+  const clickButton = (className) => {
+    const button = document.getElementsByClassName(className)[0];
+    if (button) button.click();
+  };
+
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (!mutation.addedNodes.length) return;
+      mutation.addedNodes.forEach((addedNode) => {
+        if (addedNode.className === 'ytp-ad-player-overlay') {
+          clickButton('ytp-ad-skip-button-container');
+          console.log("AD skipped.")
+        }
+        if (addedNode.className === 'ytp-ad-overlay-slot') {
+          clickButton('ytp-ad-overlay-close-button');
+          console.log("Overlay AD closed.")
+        }
+      })
+    });
+  });
+
+  const config = {
+    childList: true,
+    subtree: true,
+  };
+
+  const page = document.getElementsByTagName('ytd-app')[0];
+  observer.observe(page, config);
+  console.log("AD observer started.")
+};
